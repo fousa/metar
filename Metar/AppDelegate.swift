6f01a45ad01cb12d5a06250ed0e6a4bca7eddefa
Jelle Vandebeeck <jelle@fousa.be>
2015-10-20 10:43:33 +0200

    Setup basic Xcode project.

---
+++ Metar/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Metar
+//
+//  Created by Jelle Vandebeeck on 20/10/15.
+//  Copyright © 2015 Jelle Vandebeeck. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ MetarTests/MetarTests.swift
@@ -0,0 +1,36 @@
+//
+//  MetarTests.swift
+//  MetarTests
+//
+//  Created by Jelle Vandebeeck on 20/10/15.
+//  Copyright © 2015 Jelle Vandebeeck. All rights reserved.
+//
+
+import XCTest
+@testable import Metar
+
+class MetarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
