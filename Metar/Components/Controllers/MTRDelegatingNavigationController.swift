846fa1cf16682c3b4573b3fc70e1f519254987c5
Jelle Vandebeeck <jelle@fousa.be>
2016-01-18 21:43:21 +0100

    Prefix the delegating navigation controller component.

--- Metar/Components/Controllers/MTRDelegatingNavigationController.swift
+++ Metar/Components/Controllers/MTRDelegatingNavigationController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DelegatingNavigationController : UINavigationController {
+class MTRDelegatingNavigationController : UINavigationController {
     
     // MARK: - Status bar
     
