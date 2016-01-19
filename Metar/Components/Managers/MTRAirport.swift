bd37476be48cbdc9da5132eb4eb8127ef232519d
Jelle Vandebeeck <jelle@fousa.be>
2016-01-19 21:55:28 +0100

    Add basic data model.

---
+++ Metar/Components/Managers/MTRAirport.swift
@@ -0,0 +1,22 @@
+//
+//  MTRAirport.swift
+//  Metar
+//
+//  Created by Jelle Vandebeeck on 19/01/16.
+//  Copyright © 2016 Jelle Vandebeeck. All rights reserved.
+//
+
+import CoreData
+
+@objc(MTRAirport)
+class MTRAirport: NSManagedObject {
+
+    @NSManaged var name: String?
+    @NSManaged var country: String?
+    @NSManaged var site: String?
+    @NSManaged var longitude: NSNumber?
+    @NSManaged var latitude: NSNumber?
+    @NSManaged var elevation: NSNumber?
+    @NSManaged var rawMetarData: String?
+
+}
