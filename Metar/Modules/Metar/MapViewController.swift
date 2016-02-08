f7d833de4395ec72983c5999161566b78a242312
Jelle Vandebeeck <jelle@fousa.be>
2016-02-08 23:29:57 +0100

    Add the basic map container view.

---
+++ Metar/Modules/Metar/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  Metar
+//
+//  Created by Jelle Vandebeeck on 08/02/16.
+//  Copyright © 2016 Jelle Vandebeeck. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+}
