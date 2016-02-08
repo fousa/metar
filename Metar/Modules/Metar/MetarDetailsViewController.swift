c428efe1cbc089766714386c12fb60b3ed50057c
Jelle Vandebeeck <jelle@fousa.be>
2016-02-08 23:25:20 +0100

    Add basic metar details container.

---
+++ Metar/Modules/Metar/MetarDetailsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MetarDetailsViewController.swift
+//  Metar
+//
+//  Created by Jelle Vandebeeck on 08/02/16.
+//  Copyright © 2016 Jelle Vandebeeck. All rights reserved.
+//
+
+import UIKit
+
+class MetarDetailsViewController: UIViewController {
+}
