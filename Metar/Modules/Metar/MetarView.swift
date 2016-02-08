099425c6f5574d845d78d9bc040d13832bde5f22
Jelle Vandebeeck <jelle@fousa.be>
2016-02-08 23:36:09 +0100

    Animate the map navigation.

--- Metar/Modules/Metar/MetarView.swift
+++ Metar/Modules/Metar/MetarView.swift
@@ -13,6 +13,7 @@ import AnimatedSegmentSwitch
 class MetarView: UIView {
 
     @IBOutlet private var segmentedSwitch: AnimatedSegmentSwitch!
+    @IBOutlet private var leadingMapConstraint: NSLayoutConstraint!
 
     // MARK: - View flow
 
@@ -30,14 +31,19 @@ class MetarView: UIView {
             NSLocalizedString("detail_segment_label_metar", comment: ""),
             NSLocalizedString("detail_segment_label_map", comment: "")
         ]
-
-
     }
 
     // MARK: - Action
 
     @IBAction func switchedSegment(sender: AnyObject) {
         print("🎯 Switch to segment \(segmentedSwitch.selectedIndex)")
+
+        let presentMap = segmentedSwitch.selectedIndex == 1
+        leadingMapConstraint.priority = presentMap ? 800.0 : 600.0
+        // Use all the segmented variables in order to obtain the same animation result.
+        UIView.animateWithDuration(segmentedSwitch.animationDuration, delay: 0.0, usingSpringWithDamping: segmentedSwitch.animationSpringDamping, initialSpringVelocity: segmentedSwitch.animationInitialSpringVelocity, options: [], animations: { finished in
+            self.layoutIfNeeded()
+        }, completion: nil)
     }
 
 }
